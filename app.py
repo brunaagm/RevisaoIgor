400e90c5a6881829e330a5cefc8419dfb23e0484
brunaagm <135059772+brunaagm@users.noreply.github.com>
2024-11-05 00:16:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, session, render_template, redirect, request, url_for
+from controllers.questionarioController import questionarioController
 
 app = Flask(__name__)
+app.secret_kay = "brunalinda"
 
-@app.route("/")
-def index ():
-    return render_template("index.html")
+rotas_publicas = ["questoes.index", "questoes.verifica"]
+
+@app.before_request #criação do middleware
+def verificarIdentificação():
+    if request.endpoint in rotas_publicas: # O endpoint é basicamente a junção das questoes (blueprint) com a função
+        return
+    
+app.register_blueprint(questionarioController)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
 
 
